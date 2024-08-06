f87410d5d5a31215eac6b8fa5bfc10de26c73edd
GoetzDeBouville <count.of.tuscan@gmail.com>
2024-08-06 23:42:49 +0300

    add animation examples for change for changing colors elems

--- composeApp/src/commonMain/kotlin/org/company/app/App.kt
+++ composeApp/src/commonMain/kotlin/org/company/app/App.kt
@@ -1,46 +1,95 @@
 package org.company.app
 
+import androidx.compose.animation.Animatable
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.Image
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.platform.LocalUriHandler
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
-import composeanimationxmpl.composeapp.generated.resources.*
+import kotlinx.coroutines.delay
 import org.company.app.theme.AppTheme
-import org.company.app.theme.LocalThemeIsDark
-import kotlinx.coroutines.isActive
-import org.jetbrains.compose.resources.Font
-import org.jetbrains.compose.resources.stringResource
-import org.jetbrains.compose.resources.vectorResource
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun App() = AppTheme {
-    ColorAnimationExample()
+    val modifier = Modifier
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Мой заголовок")
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                println("Clicked")
+            }) {
+                Icon(Icons.Default.Add, contentDescription = "Добавить")
+            }
+        }
+    ) { innerPadding ->
+        Column(modifier = modifier.padding(innerPadding)) {
+            Text(
+                modifier = modifier.padding(16.dp, top = 16.dp),
+                text = "Привет, мир!"
+            )
+            ColorAnimationExample(modifier)
+            TimedColorChangeAnimation(modifier)
+        }
+    }
+}
+
+@Composable
+fun ColorAnimationExample(modifier: Modifier) {
+    var isClicked by remember { mutableStateOf(false) }
+    val backgroundColor by animateColorAsState(
+        targetValue = if (isClicked) Color.Red else Color.Blue,
+        animationSpec = tween(durationMillis = 2000)
+    )
+
+    Box(
+        modifier = modifier
+            .padding(start = 16.dp)
+            .size(100.dp)
+            .background(backgroundColor)
+            .clickable { isClicked = !isClicked }
+    )
 }
 
 
 @Composable
-fun ColorAnimationExample() {
-    var isClicked by remember { mutableStateOf(false) }
-    val backgroundColor by animateColorAsState(
-        targetValue = if (isClicked) Color.Red else Color.Blue
-    )
+fun TimedColorChangeAnimation(modifier: Modifier) {
+    val colors = listOf(Color.Red, Color.Green, Color.Blue, Color.Yellow)
+    var currentColorIndex by remember { mutableStateOf(0) }
+
+    val backgroundColor = remember { Animatable(colors[currentColorIndex]) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(2000)
+            currentColorIndex = (currentColorIndex + 1) % colors.size
+            backgroundColor.animateTo(
+                targetValue = colors[currentColorIndex],
+                animationSpec = tween(durationMillis = 1000)
+            )
+        }
+    }
 
     Box(
         modifier = Modifier
+            .padding(start = 16.dp, top = 16.dp)
             .size(100.dp)
-            .background(backgroundColor)
-            .clickable { isClicked = !isClicked }
+            .background(backgroundColor.value)
     )
 }
